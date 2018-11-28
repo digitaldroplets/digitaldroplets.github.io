642bdf72b6fb6ec4cd166be2d6f63197d0f9f742
Jeremy Merrill <jeremy.j.merrill@gmail.com>
2018-11-27 17:23:58 -0700

    contact instructions

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => (
     <h1>Hello.</h1>
     <p>We build software applications and interactive experiences for the web and mobile.</p>
     <p>Please excuse our present appearance while we build our own web site.</p>
+    <p>In the meantime, please send an email to "info" at this domain (with no "www." in front) if you&#39;d like to contact us.</p>
     <p>Thank you for visiting.</p>
   </Layout>
 )
